67d2b144b6ee39f68418ea8d98e7bf07b81afdbd
Darkshad0w <65877605+armuh16@users.noreply.github.com>
2022-02-17 21:48:28 +0800

    Model View Contrroller

--- app/application.go
+++ app/application.go
@@ -1,1 +1,11 @@
 package app
+
+import "github.com/gin-gonic/gin"
+
+var (
+	router = gin.Default()
+)
+
+func StartApplication() {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+// import (
+// 	"github.com/armuh16/book_user-api/app"
+// )
 
-}
+// func main() {
+// 	app.StartApplication()
+
+// }
