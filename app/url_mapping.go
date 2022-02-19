430f5003dc64104066e038becfa76cb114660d61
Darkshad0w <65877605+armuh16@users.noreply.github.com>
2022-02-19 14:28:24 +0800

    Model View Controller

--- app/url_mapping.go
+++ app/url_mapping.go
@@ -5,5 +5,9 @@ import (
 )
 
 func mapUrls() {
-	router.GET("/ping", controllers.Ping)
+	router.GET("/ping", controllers.Ping) // Path
+
+	router.GET("/users/:user_id", controllers.GetUser)
+	router.GET("/users/search", controllers.SearchUser)
+	router.POST("/users", controllers.CreateUser)
 }
--- controllers/ping_controller.go
+++ controllers/ping_controller.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Ping(c *gin.Context) {
-	c.String(http.StatusOK, "pong")
+	c.String(http.StatusOK, "pong") // format
 }
 
 const (
