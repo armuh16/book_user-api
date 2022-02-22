89eac2b112fa9af18a618e560e5cccf8f14c8dfb
Darkshad0w <65877605+armuh16@users.noreply.github.com>
2022-02-22 16:19:40 +0800

    Model View Controller

--- app/application.go
+++ app/application.go
@@ -1,12 +1,12 @@
 package app
 
-import "github.com/gin-gonic/gin"
+// import "github.com/gin-gonic/gin"
 
-var (
-	router = gin.Default()
-)
+// var (
+// 	router = gin.Default()
+// )
 
-func StartApplication() {
-	mapUrls()
-	router.Run(":8081") //address
-}
+// func StartApplication() {
+// 	mapUrls()
+// 	router.Run(":8081") //address
+// }
--- app/url_mapping.go
+++ app/url_mapping.go
@@ -1,13 +1,13 @@
 package app
 
-import (
-	"github.com/armuh16/book_user-api/controllers"
-)
+// import (
+// 	"github.com/armuh16/book_user-api/controllers/ping_controller"
+// )
 
-func mapUrls() {
-	router.GET("/ping", controllers.Ping) // Path
+// func mapUrls() {
+// 	router.GET("/ping", controllers.Ping) // Path
 
-	router.GET("/users/:user_id", controllers.GetUser)
-	router.GET("/users/search", controllers.SearchUser)
-	router.POST("/users", controllers.CreateUser)
-}
+// 	router.GET("/users/:user_id", controllers.GetUser)
+// 	router.GET("/users/search", controllers.SearchUser)
+// 	router.POST("/users", controllers.CreateUser)
+// }
