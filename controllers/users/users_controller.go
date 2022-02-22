e8dcb7a263f51d045cfed989bfd3d20255324995
Darkshad0w <65877605+armuh16@users.noreply.github.com>
2022-02-22 16:25:36 +0800

    ORM

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,1 +1,23 @@
 package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	counter int
+)
+
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me")
+}
+
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me")
+}
+
+func SearchUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me")
+}
