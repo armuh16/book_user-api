71608ba9a9addc6234d68bcbc70828c9fe6ee4e6
Darkshad0w <65877605+armuh16@users.noreply.github.com>
2022-02-22 16:28:54 +0800

    ORM

--- app/url_mapping.go
+++ app/url_mapping.go
@@ -1,13 +1,14 @@
 package app
 
-// import (
-// 	"github.com/armuh16/book_user-api/controllers/ping_controller"
-// )
+import (
+	pingcontroller "github.com/armuh16/book_user-api/controllers/ping"
+	"github.com/armuh16/book_user-api/controllers/users"
+)
 
-// func mapUrls() {
-// 	router.GET("/ping", controllers.Ping) // Path
+func mapUrls() {
+	router.GET("/ping", pingcontroller.Ping) // Path
 
-// 	router.GET("/users/:user_id", controllers.GetUser)
-// 	router.GET("/users/search", controllers.SearchUser)
-// 	router.POST("/users", controllers.CreateUser)
-// }
+	router.GET("/users/:user_id", users.GetUser)
+	router.GET("/users/search", users.SearchUser)
+	router.POST("/users", users.CreateUser)
+}
