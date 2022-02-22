b3107d754040a5e9e5cf83b742c53f3b3b780e1b
Darkshad0w <65877605+armuh16@users.noreply.github.com>
2022-02-22 16:54:56 +0800

    ORM

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,8 +1,11 @@
 package users
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/armuh16/book_user-api/domain/users"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +14,15 @@ var (
 )
 
 func CreateUser(c *gin.Context) {
+	var user users.User
+	fmt.Println(user)
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		// TODO: Handle Error
+		return
+	}
+	fmt.Println(string(bytes))
+	fmt.Println(err)
 	c.String(http.StatusNotImplemented, "implement me")
 }
 
