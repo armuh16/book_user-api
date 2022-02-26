8ec244a3cd31192234f01eab4e4064ee929b51e7
Darkshad0w <65877605+armuh16@users.noreply.github.com>
2022-02-26 13:15:04 +0800

    ORM

--- services/users_service.go
+++ services/users_service.go
@@ -10,7 +10,7 @@ import (
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	// return nil, nil
 
-	return &user, nil
+	// return &user, nil
 
 	return &user, &errors.RestErr{
 		Status: http.StatusInternalServerError,
