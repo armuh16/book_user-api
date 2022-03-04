a96f5e605f271131367a99de1f94434c3c6d7901
armuh16 <muhammadarif.aq@icloud.com>
2022-03-05 00:16:03 +0800

    ORM

--- domain/users/user.go
+++ domain/users/user.go
@@ -14,13 +14,13 @@ type User struct {
 	DateCreated string `json:"date_created"`
 }
 
-func Validate(user *User) *errors.RestErr {
-	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
-	if user.Email == "" {
-		return errors.NewBadRequestError("invalid email address")
-	}
-	return nil
-}
+// func Validate(user *User) *errors.RestErr {
+// 	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+// 	if user.Email == "" {
+// 		return errors.NewBadRequestError("invalid email address")
+// 	}
+// 	return nil
+// }
 
 func (user *User) Validate() *errors.RestErr {
 	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
