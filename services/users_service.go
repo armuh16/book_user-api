6ff494f2db495124d24b6c6054ebfd0bd5677e5b
armuh16 <muhammadarif.aq@icloud.com>
2022-03-05 00:08:23 +0800

    ORM

--- services/users_service.go
+++ services/users_service.go
@@ -1,8 +1,6 @@
 package services
 
 import (
-	"net/http"
-
 	"github.com/armuh16/book_user-api/domain/users"
 	"github.com/armuh16/book_user-api/utils/errors"
 )
@@ -12,9 +10,10 @@ func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 
 	// return &user, nil
 
-	return &user, &errors.RestErr{
-		Status: http.StatusInternalServerError,
-	}
+	return nil, nil
+	// return &user, &errors.RestErr{
+	// 	Status: http.StatusInternalServerError,
+	// }
 }
 
 func GetUser() {
